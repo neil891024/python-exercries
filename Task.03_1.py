c259852b38c7b79f25ca7ce99c811d0ba54d071e
neil891024 <99018615+neil891024@users.noreply.github.com>
2023-03-20 19:50:39 +0800

    Create Task.03_1.py

---
+++ Task.03_1.py
@@ -0,0 +1,6 @@
+#Task #03 作業 02【實作題】字串取代
+#需輸入包含 not that poor 的字串
+
+s = input() # The company is not that poor! 
+new_s = s.replace("not that poor","good") #將字串替換為good
+print(new_s) # The company is good! 
